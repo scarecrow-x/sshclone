5bd16d1b6c2db48216eb76c2266f7a082d33d917
徐维 <446174931@qq.com>
2021-05-06 15:45:17 +0800

    1

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: "sshclone",
+      script: "bin/www.js",
+      env: {
+        COMMON_VARIABLE: "true",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      user: "root",
+      host: "47.115.35.65",
+      ref: "origin/master",
+      repo: "git@github.com:scarecrow-x/sshclone.git",
+      path: "/var/www/development",
+      "post-deploy":
+        "npm install && pm2 startOrRestart ecosystem.json --env dev",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  },
+};
